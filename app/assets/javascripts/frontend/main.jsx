2857fe745da36d459cb90dfbae26cc658d87b888
Oskar Niburski <oskarniburski@gmail.com>
2015-12-02 20:52:36 -0500

    Initial setup for react

---
+++ app/assets/javascripts/frontend/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let notes = [
+  {id: 1, body: 'english text'},
+  {id: 2, body: 'other text'},
+]
+
+React.render(
+  <h1>test</h1>, 
+  document.getElementById('hello')
+);
