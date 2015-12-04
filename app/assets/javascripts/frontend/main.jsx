89ff4354161be859497487a0dc7e2307b1ba5946
Oskar Niburski <oskarniburski@gmail.com>
2015-12-03 23:09:45 -0500

    Other half with views

--- app/assets/javascripts/frontend/main.jsx
+++ app/assets/javascripts/frontend/main.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import PostStore from "./stores/PostStore"
+import PostList from "./components/PostList"
 import PostActions from "./actions/PostActions"
+
 PostActions.getAllPosts();
+
+let getAppState = () => {
+  return { postsList: PostStore.getAll() };
+}
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { postsList: []}
+    this.state = getAppState();
+    // need to manually bind to this on e6
+    this._onChange = this._onChange.bind(this)
+  }
+
+  componentDidMount() {
+    PostStore.addChangeListener(this._onChange)
+  }
+
+  componentWillUnmount() {
+    PostStore.removeChangeListener(this._onChange)
+  }
+
+  _onChange() {
+    this.setState(getAppState())
   }
 
   render() {
-    return <div> test </div>
+    return (
+      <PostList posts={this.state.postsList}/>
+    )
   }
 }
 
--- app/assets/javascripts/frontend/stores/PostStore.jsx
+++ app/assets/javascripts/frontend/stores/PostStore.jsx
@@ -1,11 +1,39 @@
 import AppDispatcher from "../dispatcher";
 import ActionTypes from "../constants"
+import {EventEmitter} from "events";
+
+
+let _posts = [];
+
+class PostEventEmitter extends EventEmitter {
+  getAll() {
+    return _posts;
+  }
+
+  emitChange() {
+    this.emit("CHANGE");
+  }
+
+  addChangeListener(callback) {
+    this.on("CHANGE", callback)
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener("CHANGE", callback);
+  }
+}
+
+let PostStore = new  PostEventEmitter();
 
 AppDispatcher.register( action => {
   // action.actionType === RECEIVED_POSTS
   switch(action.actionType) {
     case ActionTypes.RECEIVED_POSTS:
+      _posts = action.rawPosts;
+      PostStore.emitChange()
       break;
     default:
   }
 })
+
+export default PostStore;
