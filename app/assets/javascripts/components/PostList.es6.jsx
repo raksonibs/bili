5ae479627ad76c9a75e1859b272353a5970680b4
Oskar Niburski <oskarniburski@gmail.com>
2016-02-12 15:47:08 -0500

    Posts on load

--- app/assets/javascripts/components/PostList.es6.jsx
+++ app/assets/javascripts/components/PostList.es6.jsx
@@ -1,8 +1,27 @@
+let posts = []
+
 class PostList extends React.Component {
+  constructor(props) {
+    super(props);
+    component = this;
+    
+    this.state = {
+      posts: this.props.posts || posts
+    }
+  }
+
+  componentDidMount() {
+
+  }
   render() {    
     return (
       <div>
-        <Post spanish_text={"hola"} english_text={"hi"} />
+      { 
+        this.state.posts.map((post, index) => {
+          return <Post spanish_text={post.spanish_text} english_text={post.english_text} key={index} />
+        })
+        
+      }
       </div>
     ) 
   }
