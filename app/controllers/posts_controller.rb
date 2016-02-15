e9d82df1df975c4f4fb3f66dfc498fa3f6b5b219
Oskar Niburski <oskarniburski@gmail.com>
2016-02-14 19:02:03 -0500

    Corresponding routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,16 @@ class PostsController < ApplicationController
   def index
     render json: Post.all
   end
+
+  def music
+    render json: Post.where(type: 'Music')
+  end
+
+  def books
+    render json: Post.where(type: 'Book')
+  end
+
+  def articles
+    render json: Post.where(type: 'Article')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'home/index'
+  
+  resources :posts do 
+    collection do 
+      get '/music' => 'posts#music'
+      get '/books' => 'posts#books'
+      get '/articles' => 'posts#articles'
+    end
+  end
 
+  get 'home/index'
   root 'home#index'
 end
