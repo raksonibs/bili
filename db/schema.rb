d004d1a2b174a82efbb9634a861bfa3dc5a1f1ca
Oskar Niburski <oskarniburski@gmail.com>
2016-02-15 11:18:53 -0500

    Title migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214234550) do
+ActiveRecord::Schema.define(version: 20160215161728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160214234550) do
     t.string   "audio_file"
     t.string   "type"
     t.integer  "page_number"
+    t.string   "title"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,21 @@ Post.create(
   spanish_text: 'He estado recordando los momentos que te di, Cuántos tu me diste y porque ahora estoy aquí, Sentada en el suelo pensando que te quiero, Que te quise tanto, y que tu amor me es necesario, Déjenme llorar, Quiero sacarlo de mi pecho, Con mi llanto apagar, Este fuego que arde adentro, Déjenme llorar, Quiero despedirme en silencio, Hacer mi mente razonar, Que para esto no hay remedio, Fueron tan bellos encuentros, Amarnos sin miedo, Eres tu la noche y yo tu sueño, Tu mi cuenta cuentos, Te olvidare lo juro, lo siento, Tu amor me hace daño, Y esto no puedes ya arreglarlo, Pero amor como el mío no hallarás por ahí, Porque este amor apuesta hasta por mi.', 
   english_text: "I've been remembering the moments that I gave to you, How many you gave to me and why I'm now here, Sat on the floor thinking that I love you, That I loved you so much, and that for me your love is necessary, Let me cry, I want to get this off my chest, With my tears extinguish, This fire that burns inside, Let me cry, I want to say goodbye in silence, To make my mind see sense, That for this there is no solution, They were such beutiful encounters, To love each other without fear, You are the night and I your dream, You my story-teller, I will forget you, I swear it, I'm sorry, Your love causes me harm, And this you cannot fix, But a love like mine will not be found there, For even this love is on my side.",
   type: 'Music',
-  audio_file: nil
+  audio_file: nil,
+  title: 'Song of Songs'
 )
 
 Post.create(
   spanish_text: 'He estado recordando los momentos que te di, Cuántos tu me diste y porque ahora estoy aquí, Sentada en el suelo pensando que te quiero, Que te quise tanto, y que tu amor me es necesario, Déjenme llorar, Quiero sacarlo de mi pecho, Con mi llanto apagar, Este fuego que arde adentro, Déjenme llorar, Quiero despedirme en silencio, Hacer mi mente razonar, Que para esto no hay remedio, Fueron tan bellos encuentros, Amarnos sin miedo, Eres tu la noche y yo tu sueño, Tu mi cuenta cuentos, Te olvidare lo juro, lo siento, Tu amor me hace daño, Y esto no puedes ya arreglarlo, Pero amor como el mío no hallarás por ahí, Porque este amor apuesta hasta por mi.', 
   english_text: "I've been remembering the moments that I gave to you, How many you gave to me and why I'm now here, Sat on the floor thinking that I love you, That I loved you so much, and that for me your love is necessary, Let me cry, I want to get this off my chest, With my tears extinguish, This fire that burns inside, Let me cry, I want to say goodbye in silence, To make my mind see sense, That for this there is no solution, They were such beutiful encounters, To love each other without fear, You are the night and I your dream, You my story-teller, I will forget you, I swear it, I'm sorry, Your love causes me harm, And this you cannot fix, But a love like mine will not be found there, For even this love is on my side.",
   type: 'Book',
-  page_number: 1
+  page_number: 1,
+  title: 'Book the Book'
 )
 
 Post.create(
   spanish_text: 'He estado recordando los momentos que te di, Cuántos tu me diste y porque ahora estoy aquí, Sentada en el suelo pensando que te quiero, Que te quise tanto, y que tu amor me es necesario, Déjenme llorar, Quiero sacarlo de mi pecho, Con mi llanto apagar, Este fuego que arde adentro, Déjenme llorar, Quiero despedirme en silencio, Hacer mi mente razonar, Que para esto no hay remedio, Fueron tan bellos encuentros, Amarnos sin miedo, Eres tu la noche y yo tu sueño, Tu mi cuenta cuentos, Te olvidare lo juro, lo siento, Tu amor me hace daño, Y esto no puedes ya arreglarlo, Pero amor como el mío no hallarás por ahí, Porque este amor apuesta hasta por mi.', 
   english_text: "I've been remembering the moments that I gave to you, How many you gave to me and why I'm now here, Sat on the floor thinking that I love you, That I loved you so much, and that for me your love is necessary, Let me cry, I want to get this off my chest, With my tears extinguish, This fire that burns inside, Let me cry, I want to say goodbye in silence, To make my mind see sense, That for this there is no solution, They were such beutiful encounters, To love each other without fear, You are the night and I your dream, You my story-teller, I will forget you, I swear it, I'm sorry, Your love causes me harm, And this you cannot fix, But a love like mine will not be found there, For even this love is on my side.",
-  type: 'Article'
+  type: 'Article',
+  title: 'Live from Fiction world'
 )
