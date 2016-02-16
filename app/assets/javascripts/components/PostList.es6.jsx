51f1b5f1879dcfeff89898c6d091af5d0387ca72
Oskar Niburski <oskarniburski@gmail.com>
2016-02-16 11:34:04 -0500

    Merge errors

--- app/assets/javascripts/components/PostList.es6.jsx
+++ app/assets/javascripts/components/PostList.es6.jsx
@@ -1,9 +1,23 @@
 class PostList extends React.Component {
-  render() {    
-    return (
-      <div>
-        test
-      </div>
-    ) 
-  }
-}
+ constructor(props) {
+   super(props);
+   component = this;
+   this.state = {
+     posts: this.props.posts || posts
+   }
+   componentDidMount() {
+     
+   }
+
+   render() {    
+     return (
+       <div>
+       { 
+         this.state.posts.map((post, index) => {
+           return <Post spanish_text={post.spanish_text} english_text={post.english_text} key={index} />
+         })
+       }
+       </div>
+       )
+   }
+ }
